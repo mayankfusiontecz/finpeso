8d2b2aaf2603d39a4633772ba0c4c70b72e5d94c
Mayank-Chaudhary <mchaudhary0000@gmail.com>
2025-02-21 21:30:33 +0530

    fixes

--- components/aboutandcontacthero/herosection.jsx
+++ components/aboutandcontacthero/herosection.jsx
@@ -13,7 +13,7 @@ export default function HeroSection(props){
                 <Navbar activePath={activePath} setActivePath={setActivePath}/>
                 <div className="flex items-center justify-between gap-[4rem]  py-[8%] px-[8%]">
                                     <div className="py-10 w-[40%]">
-                                        <h1 className="text-[3.2rem] text-white font-bold w-[100%] max-md:text-[1rem] max-lg:text-[2rem]">{props.heading}</h1>
+                                        <h1 className="text-[3.2rem] text-white font-bold w-[100%] max-md:text-[1.5rem] max-lg:text-[2rem]">{props.heading}</h1>
                                         {props.subhead.split("").map((char, index) => (
                             <motion.span
                                 key={index}
@@ -24,7 +24,7 @@ export default function HeroSection(props){
                                     duration: 0.4,
                                     ease: "easeOut",
                                 }}
-                                 className="text-[2.5rem] text-[#1EC9E4] font-bold w-[80%] max-sm:text-[1rem]"
+                                 className="text-[2.5rem] text-[#1EC9E4] font-bold w-[80%] max-md:text-[1.5rem]"
                             >
                                 {char === " " ? "\u00A0" : char}
                             </motion.span>
@@ -32,7 +32,7 @@ export default function HeroSection(props){
                                     </div>
                                     {
                                         props.img1 && (
-                                            <div className='w-[50%] max-md:w-[70%]'>
+                                            <div className='w-[50%] max-md:w-[60%]'>
                                             <img src={props.img1} alt=''/>
                                             </div>
                                         )
