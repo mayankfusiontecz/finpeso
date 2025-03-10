6d76541d2d5ae2604b640f11b695651e9f58f3fa
Mayank-Chaudhary <mchaudhary0000@gmail.com>
2025-03-10 23:00:56 +0530

    fixes

--- components/aboutandcontacthero/herosection.jsx
+++ components/aboutandcontacthero/herosection.jsx
@@ -13,9 +13,8 @@ export default function HeroSection(props){
                 <Navbar activePath={activePath} setActivePath={setActivePath}/>
                 <div className="flex items-center justify-between gap-[4rem] py-[8%] px-[8%] max-md:gap-[1rem] max-md:px-[4%]">
                                     <div className="py-10 w-[40%]">
-                                        <h1 className="text-[2.5rem] text-white font-bold w-[100%] max-md:text-[1.2rem] max-lg:text-[1.5rem] uppercase">{props.heading}</h1>
+                                        <h1 className="text-[2.5rem] text-white font-bold w-[100%] max-md:text-[1rem] max-lg:text-[1.5rem] uppercase">{props.heading}</h1>
                                         
-                                        <div className="w-[80%] inline-block">
                             {props.subhead.split("").map((char, index) => (
                                 <motion.span
                                     key={index}
@@ -27,12 +26,10 @@ export default function HeroSection(props){
                                         ease: "easeOut",
                                     }}
                                     className="text-[2.5rem] text-[#1EC9E4] font-bold max-md:text-[1rem] max-lg:text-[1.2rem] uppercase"
-                                    style={{ display: 'inline' }} // Ensuring inline behavior
                                 >
                                     {char === " " ? "\u00A0" : char}
                                 </motion.span>
                             ))}
-                        </div>
                         
                         {/* <p  className="text-[2.5rem] text-[#1EC9E4] font-bold w-[90%] max-md:[100%] animate-fade max-md:text-[0.9rem] max-lg:text-[1.2rem]">{props.subhead}</p> */}
                                     </div>
